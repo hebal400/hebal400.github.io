15b19c15ea1d128375ccc14dccf6d2dff172f7c3
aerain <j40f893@gmail.com>
2019-01-14 18:05:05 +0900

    Updates

---
+++ precache-manifest.f927322ff4da33c041ff988c34793002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f67faf97d3513c23dd6",
+    "url": "/static/css/main.ba5028eb.chunk.css"
+  },
+  {
+    "revision": "1f67faf97d3513c23dd6",
+    "url": "/static/js/main.1f67faf9.chunk.js"
+  },
+  {
+    "revision": "c73afa9aa96b0b8a5d15",
+    "url": "/static/js/1.c73afa9a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "248606b77cfe529c22420cda9bfa0ee7",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54fb47dc4431dc2106cad0ab17695b70.js"
+  "/precache-manifest.f927322ff4da33c041ff988c34793002.js"
 );
 
 workbox.clientsClaim();
