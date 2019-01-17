ae532a3d4232bef9ecb8f1f9ae356bc2c80e73c8
박윤지 <bearji0623@gmail.com>
2019-01-17 11:00:40 +0900

    memo

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Redirect, NavLink } from 'react-router-dom';
 import { IoIosClipboard, IoIosCalculator } from 'react-icons/io';
-
+// import 'https://fonts.googleapis.com/css?family=Jua';
 import './css/Main.css';
 import Profile from './Profile';
 import Send from './Send';
@@ -37,10 +37,29 @@ export default class Main extends Component {
               </NavLink>
             </li>
           </ul>
+
+          <div id="wrap">
+            <label for="working-hour" className="label">근무시간</label>
+            <input name="working-hour" className="text working-hour" />
+            <br />
+
+            <label for="pay" className="label">급여</label>
+            <input name="pay" className="text pay" />
+            <br />
+
+            <label type="number" for="callnumber" className="label">연락처</label>
+            <input name="callnumber" className="text callnumber" />
+            <br />
+
+            <label for="more-details" className="label">추가메모</label>
+            <input type="text" name="more-details" id="more-details" />
+
+          </div>
+
           <Route exact path={`${match.path}`} component={Send} />
           <Route path={`${match.path}/test`} component={Calculator} />
+
         </div>
     )
   }
 }
-
