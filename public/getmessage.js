ed8d9a287a5070efde4175f8e2fc0fa41f7c9f50
aerain <j40f893@gmail.com>
2019-01-18 09:59:41 +0900

    parent로 데이터 요청

--- public/getmessage.js
+++ public/getmessage.js
@@ -1,20 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('온로드')
     window.addEventListener('message', e => {
         // 카카오 api 메시지와 충돌 방지
-        if(e.origin !== 'https://kapi.kakao.com') {
+        if(!(e.origin == 'https://kapi.kakao.com' || e.origin == 'https://kauth.kakao.com')) {
             let { data } = e;
-            console.log(e);
 
             // 부모 윈도우에서 origin 을 쏴줘서 실제 쏴준 곳과 일치하는지 확인
             // 크롬 확장에서 보낸 것을 확인용 테스트.
             if( data.origin && data.origin === e.origin) {
-
+                console.log(data);
                 // 일단 테스트로 글로벌로 적용
                 window.parsedData = data.parsedData;
             }
         }
     }, false);
-
-    window.parent.postMessage('test', '*');
+    // DOM 호출 원할때
+    window.parent.postMessage('requestDataFromParent', '*');
 });
