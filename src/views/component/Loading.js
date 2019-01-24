9f0a6a6c093f45b28a45ba2aab13a159bb7db3bb
박윤지 <bearji0623@gmail.com>
2019-01-24 13:55:07 +0900

    loading

--- src/views/component/Loading.js
+++ src/views/component/Loading.js
@@ -8,13 +8,24 @@ export default class Loading extends Component {
     render() {
         if(this.props.isAuthed === undefined) {
             return (
-                <div className="loading">
-                    로딩중이에오...
-                </div>
+              <div class="sk-circle">
+                <div class="sk-circle1 sk-child"></div>
+                <div class="sk-circle2 sk-child"></div>
+                <div class="sk-circle3 sk-child"></div>
+                <div class="sk-circle4 sk-child"></div>
+                <div class="sk-circle5 sk-child"></div>
+                <div class="sk-circle6 sk-child"></div>
+                <div class="sk-circle7 sk-child"></div>
+                <div class="sk-circle8 sk-child"></div>
+                <div class="sk-circle9 sk-child"></div>
+                <div class="sk-circle10 sk-child"></div>
+                <div class="sk-circle11 sk-child"></div>
+                <div class="sk-circle12 sk-child"></div>
+              </div>
             )
         } else {
             if(!this.props.isAuthed) return <Redirect to="/login" />
             else return <Redirect to="/send" />
-        }   
+        }
     }
 }
