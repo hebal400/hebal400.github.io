2c3a766ce1a31f280c2d9481cdba608541ec345a
aerain <j40f893@gmail.com>
2019-01-29 10:13:25 +0900

    리다이렉션 수정, 개발자 소개 하이퍼 링크 수정

--- src/actions/sendMessage.js
+++ src/actions/sendMessage.js
@@ -1,7 +1,6 @@
 export default function sendMessage() {
-    console.log('test');
     window.parent.postMessage({
         message: "requestParentOpenTab",
-        link: 'https://hebal400.github.io/albatalk/#teaminfo'
+        link: 'https://albatalk.github.io/#team'
     }, '*')
 }
